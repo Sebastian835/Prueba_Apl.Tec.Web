bc9d7675644284ede49365d3a6a7ae2752033146
Sebastian835 <sebastian.830@outlook.com>
2022-05-31 08:49:03 -0500

    Sesion

--- todoapp.py
+++ todoapp.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for  # librerias
+import numpy as np
 
 app = Flask(__name__)  # incializar variable
-
-
-# funcion de bienvenida
-@app.route('/')
-def index():
-    return render_template('index.html')       #pasa el parametro al index
 
 #arreglos Cliente
 Nombre_Cliente = []
 Nombre_Telefono = []
 Nombre_Estado = []
 
+# funcion de bienvenida
+@app.route('/')
+def index():
+    return render_template('index.html', NombreClienteLista = Nombre_Cliente )       #pasa el parametro al index
+
 #add_tareas
 @app.route('/registros', methods=['POST'])  # recive atravez del metodo POST
 def enviar():
    if request.method == 'POST':
-      # almacena el nombre de la tarea
+      # almacena el nombre 
       Cliente_Nombre = request.form['Nombre_Cliente']
       Cliente_Telefono = request.form['Cliente_Telefono']
       Cliente_Estado = request.form['Cliente_estado']
@@ -31,18 +31,30 @@ def enviar():
       Nombre_Telefono.append(Cliente_Telefono)
       Nombre_Estado.append(Cliente_Estado)
 
+      #print(Cliente_Nombre)
       return redirect(url_for('index'))   
 
-@app.route('/Sesion')     #segunda ruta
+
+#segunda ruta
+@app.route('/Sesion')    
 def Inicio_Sesion():
+   
     return render_template('Sesion.html')
 
-@app.route('/InicioSesion')     #segunda ruta
+
+@app.route('/Iniciar ', methods=['POST'])   
 def Iniciar():
-
-
-    return render_template('Sesion.html')
-
+    if request.method == 'POST':
+        InicioNombre = request.form['IniciarSesion']
+        lst = np.array(Nombre_Cliente)
+        result = np.where(lst == InicioNombre)
+        print("result")  
+        if(result):
+            return render_template('Sesion'):
+        else:
+            return redirect(url_for('index'))   
+    
+        #return render_template('Sesion')
 
 # main del programa
 if __name__ == '__main__':
